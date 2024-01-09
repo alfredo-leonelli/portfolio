49301f81c94034f9917c32e479d2b6469ed0ef37
Alfredo Leonelli <alfredoleonellim@gmail.com>
2024-01-09 18:52:46 -0300

    Initial Commit

---
+++ src/app/pages/landingPage.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div>
+      <div className="flex space-x-2 justify-center items-center bg-white h-96 dark:invert">
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+      </div>
+      <h1 className="text-4xl font-bold font-mono text-center">
+        Página en desarrollo
+      </h1>
+    </div>
+  );
+}
