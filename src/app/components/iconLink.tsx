dabe76c7f68beb88df7aaebc38df182541490007
Alfredo Leonelli <alfredoleonellim@gmail.com>
2024-01-22 18:53:59 -0300

    style: add mobile style and improve visibility in workItem component

--- src/app/components/iconLink.tsx
+++ src/app/components/iconLink.tsx
@@ -9,7 +9,7 @@ export default function IconLink({
     <a
       href={href}
       target="_blank"
-      className="block p-6 hover:text-[#7D8E29] hover:scale-110 transition-all duration-500 ease-in-out"
+      className="block p-6 hover:text-[#7D8E29] hover:scale-110 transition-all duration-500 ease-in-out mr-1"
       style={{
         background: "linear-gradient(to top, #7D8E29 50%, transparent 50%)",
         backgroundSize: "100% 200%",
