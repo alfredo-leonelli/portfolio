4273a59d96522e9b1adecd10382b2058601d7879
Alfredo Leonelli <alfredoleonellim@gmail.com>
2024-01-22 04:39:14 -0300

    fix: try to fix images not loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.alfredoleonelli.cl",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
