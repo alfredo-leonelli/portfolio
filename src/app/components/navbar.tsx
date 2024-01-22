2e160cf22edcdd753dff7ef82271f64a2f3d0ab5
Alfredo Leonelli <alfredoleonellim@gmail.com>
2024-01-22 04:18:44 -0300

    Revert "fix: try to fix pdf redirect"
    This reverts commit e4e07fe35307d4493a05331d4b4ab0ba2ca2b3f4.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
       <div>
         <ul className="flex">
           <li>
-            <IconLink href={process.env.PUBLIC_URL + "/CVAlfredoLeonelli.pdf"}>
+            <IconLink href="mailto:alfredoleonellim@gmail.com">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
